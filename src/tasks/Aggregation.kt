ea0ec8a69238efc8c4391647a89a3c0d002b0199
minseoklim <ddalgu2@gmail.com>
2024-07-03 20:03:55 +0900

    solve Task 1

--- src/tasks/Aggregation.kt
+++ src/tasks/Aggregation.kt
@@ -14,5 +14,8 @@ TODO: Write aggregation code.
  The corresponding test can be found in test/tasks/AggregationKtTest.kt.
  You can use 'Navigate | Test' menu action (note the shortcut) to navigate to the test.
 */
-fun List<User>.aggregate(): List<User> =
-    this
+fun List<User>.aggregate(): List<User> {
+    return this.groupBy { it.login }
+        .map { User(it.key, it.value.sumOf { it.contributions }) }
+        .sortedByDescending { it.contributions }
+}
