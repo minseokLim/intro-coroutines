e0d82b23cc802b6bbd974bdf614d3dfd061da7d0
minseoklim <ddalgu2@gmail.com>
2024-07-04 13:23:25 +0900

    solve Task 5

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -3,6 +3,21 @@ package tasks
 import contributors.*
 import kotlinx.coroutines.*
 
-suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    TODO()
+suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> {
+    return coroutineScope {
+        val repos = service
+            .getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .body() ?: emptyList()
+
+        repos.map { repo ->
+            async(Dispatchers.Default) {
+                log("starting loading for ${repo.name}")
+                service
+                    .getRepoContributors(req.org, repo.name)
+                    .also { logUsers(repo, it) }
+                    .bodyList()
+            }
+        }.awaitAll().flatten().aggregate()
+    }
 }
