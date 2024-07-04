fd67b974108283ea234d1f2173753f8f8ab418d1
minseoklim <ddalgu2@gmail.com>
2024-07-04 15:32:25 +0900

    solve Task 6

--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -7,5 +7,18 @@ suspend fun loadContributorsProgress(
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: emptyList()
+
+    val users = mutableListOf<User>()
+    repos.forEachIndexed { index, repo ->
+        users += service
+            .getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+
+        updateResults(users.aggregate(), index == repos.lastIndex)
+    }
 }
