0d845762fe7e0a958207e34a2881eb49c38bc127
minseoklim <ddalgu2@gmail.com>
2024-07-04 16:23:34 +0900

    solve Task 7

--- src/tasks/Request7Channels.kt
+++ src/tasks/Request7Channels.kt
@@ -11,6 +11,27 @@ suspend fun loadContributorsChannels(
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
     coroutineScope {
-        TODO()
+        val repos = service
+            .getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .body() ?: emptyList()
+
+        val channel = Channel<List<User>>()
+        repos.forEach { repo ->
+            launch {
+                log("starting loading for ${repo.name}")
+                val users = service
+                    .getRepoContributors(req.org, repo.name)
+                    .also { logUsers(repo, it) }
+                    .bodyList()
+                channel.send(users)
+            }
+        }
+
+        val allUsers = mutableListOf<User>()
+        repeat(repos.size) {
+            allUsers += channel.receive()
+            updateResults(allUsers.aggregate(), it == repos.lastIndex)
+        }
     }
 }
